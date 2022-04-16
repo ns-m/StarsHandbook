15865449b056ed472a60272df9e4a62a0983b9ab
KMV <9117479@gmail.com>
2022-04-16 22:18:19 +0300

    NS RecyclerView Lesson26c

--- app/src/main/java/com/example/starshandbook/StarAdapter.kt
+++ app/src/main/java/com/example/starshandbook/StarAdapter.kt
@@ -2,9 +2,11 @@ package com.example.starshandbook
 
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import com.example.starshandbook.databinding.StarItemBinding
 
 class StarAdapter: RecyclerView.Adapter<StarAdapter.StarHolder> {
     class StarHolder(item: View):RecyclerView.ViewHolder(item) {
+        val binding = StarItemBinding.bind(item)
         fun bind(star: Star){
 
         }
