0c5468884173e84b3cef97d295584e582e51d028
KMV <9117479@gmail.com>
2022-04-17 22:51:25 +0300

    NS RecyclerView Lesson26d

--- app/src/main/java/com/example/starshandbook/MainActivity.kt
+++ app/src/main/java/com/example/starshandbook/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.starshandbook
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.starshandbook.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    private val adapter = StarAdapter()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        init()
+    }
+    private fun init(){
+        binding.apply {
+            recyclerViewStars.layoutManager = GridLayoutManager(this@MainActivity, 3)
+            recyclerViewStars.adapter = adapter
+        }
     }
 }
--- app/src/main/java/com/example/starshandbook/StarAdapter.kt
+++ app/src/main/java/com/example/starshandbook/StarAdapter.kt
@@ -1,14 +1,37 @@
 package com.example.starshandbook
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.starshandbook.databinding.StarItemBinding
 
-class StarAdapter: RecyclerView.Adapter<StarAdapter.StarHolder> {
+class StarAdapter: RecyclerView.Adapter<StarAdapter.StarHolder>() {
+    val starList = ArrayList<Star>()
     class StarHolder(item: View):RecyclerView.ViewHolder(item) {
         val binding = StarItemBinding.bind(item)
-        fun bind(star: Star){
-
+        fun bind(star: Star) = with(binding){
+            starImgView.setImageResource(star.imageId)
+            textViewTitle.text = star.title
+            textViewDescription.text = star.description
         }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StarHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.star_item, parent, false)
+        return StarHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: StarHolder, position: Int) {
+        holder.bind(starList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return starList.size
+    }
+
+    fun addStar(star: Star){
+        starList.add(star)
+        notifyDataSetChanged()
     }
 }
